b206e3c38436b76d1117760e9b8385d4fdba117c
Ricardofaky <57514274+Ricardofaky@users.noreply.github.com>
2022-09-07 12:51:37 -0500

    Añadido clientes Dto

---
+++ model/dto/ClientesDto.php
@@ -0,0 +1,62 @@
+// autor: Solórzano Zambrano Ricardo
+<?php
+
+class ClientesDto
+{
+    private $id;
+    private $nombre;
+    private $apellido;
+    private $documento;
+    private $registro;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+    }
+
+    public function getDocumento()
+    {
+        return $this->documento;
+    }
+
+    public function setDocumento($documento)
+    {
+        $this->documento = $documento;
+    }
+
+    public function getRegistro()
+    {
+        return $this->registro;
+    }
+
+    public function setRegistro($registro)
+    {
+        $this->registro = $registro;
+    }
+
+}
