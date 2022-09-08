511455a504e68fa1b9df8c3d985949057b10315a
Jean Alvarez <107156851+Jean19010@users.noreply.github.com>
2022-09-07 21:48:30 -0500

    Add files via upload

---
+++ model/dto/GorrasDto.php
@@ -0,0 +1,60 @@
+<?php
+//adaptado a gorras, falta cambiar los atributos
+class GorrasDto
+{
+    private $id;
+    private $diseno;
+    private $talla;
+    private $precio;
+    private $cantidad;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getDiseno()
+    {
+        return $this->diseno;
+    }
+
+    public function setDiseno($diseno)
+    {
+        $this->diseno = $diseno;
+    }
+
+    public function getTalla()
+    {
+        return $this->talla;
+    }
+
+    public function setTalla($talla)
+    {
+        $this->talla = $talla;
+    }
+
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+    }
+
+    public function getCantidad()
+    {
+        return $this->cantidad;
+    }
+
+    public function setCantidad($cantidad)
+    {
+        $this->cantidad = $cantidad;
+    }
+}
